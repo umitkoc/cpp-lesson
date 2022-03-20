c6763d0c623def97103cfd283d8720e9b1bad3d7
Ümit KOC <umitkoc.com@gmail.com>
2022-03-20 20:23:15 +0300

    Create candidate.hpp

---
+++ candidate.hpp
@@ -0,0 +1,23 @@
+#ifndef _CANDIDATE_
+#define _CANDIDATE
+
+#include <iostream>
+
+int startPage();
+int add();
+int print();
+int search();
+int write();
+int sorted();
+int read();
+int update();
+int remove();
+struct candidate
+{
+    std::string firstname;
+    std::string lastname;
+    // std::string number;
+    // std::string email;
+};
+
+#endif
